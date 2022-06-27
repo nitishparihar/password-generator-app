432ab01800c312b1ca1d40602d92eb570fd6ef89
Nitish Parihar <nitishp97@gmail.com>
2022-06-27 23:07:54 +0530

    Update index.js
    adding input box and improving copy functionality.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+let inputBox = document.getElementById("input-box")
 let pass1 = document.getElementById("password1")
 let pass2 = document.getElementById("password2")
 let pass3 = document.getElementById("password3")
 let pass4 = document.getElementById("password4")
-
-//let passwordLength = document.getElementById("a")
 
 
 function generateString(length) {
@@ -19,33 +18,37 @@ function generateString(length) {
 
 
 function generatePasswords() {
-    pass1.textContent = generateString(10)
-    pass2.textContent = generateString(10)
-    pass3.textContent = generateString(10)
-    pass4.textContent = generateString(10)
+    if (inputBox.value > 15) {
+        alert("Please enter value less than or equal to 15")
+    } else {
+        pass1.textContent = generateString(inputBox.value)
+        pass2.textContent = generateString(inputBox.value)
+        pass3.textContent = generateString(inputBox.value)
+        pass4.textContent = generateString(inputBox.value)
+    }
 }
 
 
 function copyPassword() {
   let copyText = document.getElementById("password1");
   navigator.clipboard.writeText(copyText.innerHTML);
-  if (copyText != "") {alert("Password Copied Successfully")}
+  if (copyText.innerHTML != "") {alert("Password Copied Successfully")}
 }
 
 function copyPassword1() {
     let copyText = document.getElementById("password2");
     navigator.clipboard.writeText(copyText.innerHTML);
-    alert("Password Copied Successfully")
+    if (copyText.innerHTML != "") {alert("Password Copied Successfully")}
 }
 
 function copyPassword2() {
     let copyText = document.getElementById("password3");
     navigator.clipboard.writeText(copyText.innerHTML);
-    alert("Password Copied Successfully")
+    if (copyText.innerHTML != "") {alert("Password Copied Successfully")}
 }
 
 function copyPassword3() {
     let copyText = document.getElementById("password4");
     navigator.clipboard.writeText(copyText.innerHTML);
-    alert("Password Copied Successfully")
+    if (copyText.innerHTML != "") {alert("Password Copied Successfully")}
 }
